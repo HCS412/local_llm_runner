95d153ea80f96a9ac09388cb70b3849bf005c54e
Brandon Brooks <bbrooksvc@gmail.com>
2025-07-06 10:31:02 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,3 @@
 from .prompt_router import is_simple_question, suggest_followup, route_prompt
-from .utils_prompt_classifier import detect_prompt_type
 from .formatting import format_step_header, truncate_output
 from .cleaning import sanitize_input
-
